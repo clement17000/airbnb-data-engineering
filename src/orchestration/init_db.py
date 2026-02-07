9de0fdb459f994265f91ffaf7e007752c0a368b6
clement.greau <greauclement@gmail.com>
2026-02-07 10:51:16 +0100

    UPDATE: ERROR CORRECTION

---
+++ src/orchestration/init_db.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import requests
+import logging
+from datetime import datetime
+from google.cloud import bigquery
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import config
+
+# -- LOGIN CONFIGURATION --
+logging = config.setup_logging("init_db")
+
+def create_schema_and_tables():
+    logging.info("Starting creation of Schema & Tables.")
+
+    try:
+        client = bigquery.Client()
+        logging.info("Connexion to BigQuery established.")
+
+        with open(config.SQL_INIT_TABLE_FILE, "r") as f:
+            sql_query = f.read()
+        
+        logging.info(f"SQL File read: {config.SQL_INIT_TABLE_FILE}")
+
+        query_job = client.query(sql_query)
+        query_job.result()
+
+        logging.info("SUCCES : Dataset & Table has been updated/created.")
+
+    except Exception as e:
+        logging.error(f"ERROR : {e}")
+        raise e
+    
+if __name__ == '__main__':
+    create_schema_and_tables()
